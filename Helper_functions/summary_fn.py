c257fcd730380a4e1cb3126739ab53cb9f359371
jyotipmahes <manish1352@gmail.com>
2018-11-05 00:10:46 -0800

    Helper function update, summary and count_na

--- Helper_functions/summary_fn.py
+++ Helper_functions/summary_fn.py
@@ -1,2 +1,23 @@
 import numpy as np
 import pandas as pd
+
+
+def summary(df):
+    """
+    input: The function takes in a data frame
+    output: It counts total number of records and unique records present in each column
+    """
+
+    print("Total number of records: ",len(df))
+    for i in df.columns:
+        print('Distinct {} in train: {}'.format(i,len(np.unique(df[i].astype('str')))))
+
+
+def count_na(df):
+    """
+    input: The function takes in a data frame
+    output: It counts number of NA values in each column and % of NA values 
+    """
+    new=pd.DataFrame(df.isnull().astype('int').sum(axis=0),columns=["NA_count"])
+    new['Percentage']=df.isnull().astype('int').sum(axis=0)*100/len(df)
+    return new
